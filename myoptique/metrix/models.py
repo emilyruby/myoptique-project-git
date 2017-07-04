1bd244d62ca1af37f6a295fbea50b7c9ca824d09
“Fraser <2192722g@student.gla.ac.uk>
2017-07-04 11:39:04 +0100

    models

--- myoptique/metrix/models.py
+++ myoptique/metrix/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Department(models.Model):
     name = models.CharField(max_length=100)
-    score = models.IntegerField(default=0, null=False)
+    part_of = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
@@ -10,9 +10,9 @@ class Department(models.Model):
 class Metric(models.Model):
     name = models.CharField(max_length=100)
     desc = models.TextField(null=False, blank=False)
-    value = models.IntegerField(default=0, null=False)
-    weight = models.IntegerField(default=1, null=False)
     department = models.ForeignKey(Department, related_name='metrics')
+    curr_val = models.CharField(default='0%', max_length=10, null=False)
+    color = models.CharField(default='green', max_length=10, null=False)
 
     def __str__(self):
         return self.name
